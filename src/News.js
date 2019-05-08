8a2d8406ee3c8da7a140647b55748368f9fa5929
Natalia Kędziora <n.m.kedziora@gmail.com>
2019-05-08 19:31:28 +0200

    Fetch React news from HN API       * use async await and effect with cleanup function

--- src/News.js
+++ src/News.js
@@ -1,34 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 
 export default function News() {
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('react hooks');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const isMounted = useRef(true);
+  const searchInputRef = useRef();
 
   useEffect(() => {
-    let current = true;
-
-    axios
-      .get(`http://hn.algolia.com/api/v1/search?query=reacthooks`)
-      .then(response => {
-        if (current) {
-          setResults(response.data.hits);
-        }
-      });
+    fetchResults();
     return () => {
-      current = false;
+      isMounted.current = false;
     };
   }, []);
+
+  const fetchResults = async () => {
+    if (isLoading) {
+      return;
+    }
+
+
+    try {
+      setIsLoading(true);
+      const response = await axios.get(`https://hn.algolia.com/api/v1/search?query=${query}`);
+      if (isMounted.current) {
+        setResults(response.data.hits);
+      }
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSearch = event => {
+    event.preventDefault();
+    fetchResults();
+  };
+
+  const handleClearSearch = () => {
+    setQuery('');
+    searchInputRef.current.focus();
+  };
 
   return (
     <>
       <h1>HN news with React Hooks</h1>
-      <ul>
-        {results.map(result => (
-          <li key={result.objectID}>
-            <a href={result.url}>{result.title}</a>
-          </li>
-        ))}
-      </ul>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={query}
+          ref={searchInputRef}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit" disabled={isLoading}>Search</button>
+        <button type="button" onClick={handleClearSearch}>Clear</button>
+      </form>
+      {isLoading ? (
+        <div>Loading results...</div>
+      ) : (
+        <ul>
+          {results.map(result => (
+            <li key={result.objectID}>
+              <a href={result.url}>{result.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+      {error && <div>{error.message}</div>}
     </>
   );
 }
